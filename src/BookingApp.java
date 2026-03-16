5ab1628e9b208852b5ec8a7858c97b6a5bf98702
Samir-dhakal <samirdhakal.9279@gmail.com>
2026-03-16 14:38:49 +0530

    UC1 completed

---
+++ src/BookingApp.java
@@ -0,0 +1,18 @@
+/**
+ * Main class - Booking app
+ *
+ * use case 1 : application entry and welcome message
+ *
+ * description : this is the entry point of the hotel booking management system
+ *
+ * this application start execution from main() method, display the welcome message and print the status of system
+ *
+ * @author Samir
+ * @version 1.0
+ */
+public class BookingApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Hotel Management Booking System");
+        System.out.println("System initialized successfully");
+    }
+}
